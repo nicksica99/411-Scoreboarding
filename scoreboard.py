c317e41c3a18a3e73c70829297ba5534fdce8333
nicksica99 <52967428+nicksica99@users.noreply.github.com>
2020-11-15 23:00:09 -0500

    Add files via upload
    Updated scoreboard.py file- edits from 11/15/2020

--- scoreboard.py
+++ scoreboard.py
@@ -104,9 +104,125 @@ mem_location_18 = 167
 def readFile(filename):
     ofp = open(filename, "r")
     instructionList = ofp.readlines()
-    instruction_array = []
-
+    instructions_List = []
+    
     for i in range(len(instructionList)):
-        newInstruction = instructionList[i].split("\n")
+        instructions = instructionList[i].split("\n")
+        instructions = instructionList[i].strip("\n")
 
-        instructionList.append(newInstruction)
+        instructions_List.append(instructions)
+    
+    return instructions_List
+
+
+def printScoreboard(scoreboard):
+    print("\n  Instruction        |  Issue  | Read Operand | Execute | Write Back | ")
+
+    print(scoreboard)
+    
+def main():
+    mipsInstructions = []
+    scoreboard = []
+     
+    mipsToPython = {"Load" : "L.D", "Store": "S.D", "Add":"ADD", "AddImediate":"ADDI",
+                    "Add.d": "ADD.D", "Sub.d" : "SUB.d", "Sub" : "SUB.D", "Mul.d": "MUL.D",
+                    "Div.d": "DIV.D" }
+    #lsa = load, store, add, length of the strings
+    lsa_length = 3
+    #other instructions are 4 chars long
+    inst_length = 4
+    
+    ifp = input("Enter the filename of the MIPS instructions: ")
+    
+    mipsInstructions = readFile(ifp)
+
+    num_instruction = len(mipsInstructions)
+
+    #puts instructions into scoreboard array
+    for i in range(num_instruction):
+        scoreboard.append(mipsInstructions[i])
+
+    #this for loop will determine what the current instruction is and what to do with it
+    for i in range(num_instruction):
+        #load instructions
+        if(scoreboard[i][0] == "L"):
+            load_instruction(scoreboard[i])
+            
+        #store instructions
+        elif(scoreboard[i][0:3] == "S.D"):
+            store_instruction(scoreboard[i])
+
+        #multiply instructions
+        elif(scoreboard[i][0] == "M"):
+            multiply_instruction(scoreboard[i])
+
+        #division instructions
+        elif(scoreboard[i][0] == "D"):
+            division_instruction(scoreboard[i])
+
+        #Add instructions, there are conditions for each type
+        elif(scoreboard[i][0] == "A"):
+            if(scoreboard[i][3] == "I"):
+                add_immediate(scoreboard[i])
+            elif(scoreboard[i][4] == "D"):
+                add_fp(scoreboard[i])
+            else:
+                add_integer(scoreboard[i])
+
+        #subtraction instructions, there are conditions for each type
+        elif(scoreboard[i][0:3] == "SUB"):
+            if(scoreboard[i][4] == "D"):
+                sub_fp(scoreboard[i])
+            else:
+                sub_integer(scoreboard[i])
+            
+        
+        
+    
+ #   printScoreboard(scoreboard)
+
+# 
+# Input: takes in a Load instruction and does the arithmetic
+# Output: idk yet
+def load_instruction(instruction):
+    issue_timer = 0
+    read_operand_timer = 0
+    execution_timer = 0
+    write_back_timer = 0
+    
+    print("load")
+    for i in range(len(instruction)):
+        if(instruction[i] == "F"):
+            reg = instruction[i:i+2]
+            
+    print(reg)
+            
+    
+def store_instruction(instruction):
+    print("store")
+    
+def add_integer(instruction):
+    print("add_integer")
+
+def add_immediate(instruction):
+     print("add_immediate")
+def add_fp(instruction):
+     print("add_fp")
+
+def sub_fp(instruction):
+     print("sub_fp")
+
+def sub_integer(instruction):
+    print("sub_integer")
+def multiply_instruction(instruction):
+     print("multiply")
+
+def division_instruction(instruction):
+     print("division")
+
+main()
+    
+
+ 
+
+    
